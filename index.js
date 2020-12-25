ef429b84f1db70ead4ec5f5daed36cbcf5b28c8b
Mark Simoncic <63131950+masimoncic@users.noreply.github.com>
2020-12-24 22:47:06 -0500

    repeat calls now work properly

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 function makeList () {
     let input = document.getElementById('htmlInput1Id');
     const list = document.getElementById('list');
+    errMsg = document.getElementById('errMsg');
     let subStrs = seperateParenthesis(input.value);
+    errMsg.hidden = true;
+    list.innerHTML = '';
+    if (subStrs === null) {
+        errMsg.hidden = false;
+        return;
+    }
     for (i = 0; i < subStrs.length; i++) {
         let li = document.createElement('li');
         li.innerText = subStrs[i];
