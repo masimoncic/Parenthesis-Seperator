3cea5ea8409b417e56ea4204a8e0a366309a1ce8
Mark Simoncic <63131950+masimoncic@users.noreply.github.com>
2020-12-25 17:53:06 -0500

    added mocha browser tests

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 function makeList () {
     let input = document.getElementById('htmlInput1Id');
     const list = document.getElementById('list');
-    errMsg = document.getElementById('errMsg');
+    let errMsg = document.getElementById('errMsg');
     let subStrs = seperateParenthesis(input.value);
-    errMsg.hidden = true;
     list.innerHTML = '';
-    if (subStrs === null) {
-        errMsg.hidden = false;
-        return;
-    }
-    for (i = 0; i < subStrs.length; i++) {
+    errMsg.hidden = true;
+    if (testValidInput(subStrs) === null) {return;};
+    for (let i = 0; i < subStrs.length; i++) {
         let li = document.createElement('li');
         li.innerText = subStrs[i];
         list.appendChild(li);
     }
 }
 
-
+function testValidInput(subStrs) {
+    if (subStrs === null) {
+        errMsg.hidden = false;
+        return null;
+    }
+}
 
 const button = document.getElementById('generateOOOId');
 button.addEventListener('click', makeList);
